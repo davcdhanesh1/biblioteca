a26e9eb1f846f8a43818527b45175f1d630481be
Dhanesh Arole <dhanesha@thoughtworks.com>
2015-02-25 11:51:51 +0530

    Printing list of books

--- src/main/java/book/BookList.java
+++ src/main/java/book/BookList.java
@@ -3,13 +3,22 @@ package book;
 import java.util.ArrayList;
 
 public class BookList {
-    private ArrayList<Book> list = new ArrayList<Book>();
+    private ArrayList<Book> bookList = new ArrayList<Book>();
 
     public void add(Book book) {
-        list.add(book);
+        bookList.add(book);
     }
 
     public Integer count() {
-        return this.list.size();
+        return this.bookList.size();
+    }
+
+    @Override
+    public String toString() {
+        String result = new String();
+        for (Book book: bookList) {
+            result += book + "\n";
+        }
+        return result;
     }
 }
--- src/test/java/book/BookListTest.java
+++ src/test/java/book/BookListTest.java
@@ -1,18 +1,32 @@
 package book;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class BookListTest {
+    public static final String HARRY_POTTER_AND_THE_PHILOSOPHERS_STONE = "Harry Potter and the Philosopher's Stone";
+    public static final String HARRY_POTTER_AND_THE_CHAMBER_OF_SECRETS = "Harry Potter and the Chamber of Secrets";
+    BookList bookList;
+
+    @Before
+    public void setUp() throws Exception {
+        bookList = new BookList();
+        bookList.add(new Book(HARRY_POTTER_AND_THE_PHILOSOPHERS_STONE));
+        bookList.add(new Book(HARRY_POTTER_AND_THE_CHAMBER_OF_SECRETS));
+    }
 
     @Test
     public void testListOfBooks() throws Exception {
-        BookList bookList = new BookList();
-        bookList.add(new Book("Harry Potter and the Philosopher's Stone"));
-        bookList.add(new Book("Harry Potter and the Chamber of Secrets"));
-
         assertThat(bookList.count(), is(2));
+    }
+
+    @Test
+    public void testPrintListOfBooks() throws Exception {
+        String expectedBookListOutput = HARRY_POTTER_AND_THE_PHILOSOPHERS_STONE + "\n" +
+                                        HARRY_POTTER_AND_THE_CHAMBER_OF_SECRETS + "\n";
+        assertThat(bookList.toString(),is(expectedBookListOutput));
     }
 }
