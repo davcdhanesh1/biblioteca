342e7d61a07b1183ee558cb5c65a40c6cc23c79a
Dhanesh Arole <dhanesha@thoughtworks.com>
2015-03-03 10:37:06 +0530

    Func test for listing all the movies

---
+++ src/test/java/functional/BibliotecaAppTestForMovies.java
@@ -0,0 +1,99 @@
+package functional;
+
+import com.biblioteca.BibliotecaApp;
+import com.biblioteca.inputValidator.InputValidationException;
+import com.biblioteca.io.Printer;
+import com.biblioteca.item.ItemCanNotBeReturned;
+import com.biblioteca.item.ItemIsNotAvailableForCheckOut;
+import com.biblioteca.item.ItemNotFoundException;
+import com.biblioteca.item.book.Book;
+import com.biblioteca.item.book.BookList;
+import com.biblioteca.item.movie.Movie;
+import com.biblioteca.item.movie.MovieList;
+import com.biblioteca.item.movie.Rating;
+import com.biblioteca.library.Library;
+import com.biblioteca.menu.*;
+import org.junit.Before;
+import org.junit.Test;
+import testhelpers.StringUtil;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class BibliotecaAppTestForMovies {
+
+
+    BookList bookList;
+    private ByteArrayOutputStream outputStream;
+    private Printer printer;
+    private BibliotecaApp bibliotecaApp;
+    private String inputForSelectingBook;
+    private ByteArrayInputStream byteArrayInputStream;
+    private Scanner scanner;
+    private MenuList menuList;
+    private Library library;
+    private String inputForSelectingMenu;
+    private Book harryPotterAndPhilosophersStoneBook;
+    private Book harryPotterAndChambersOfSecretsBook;
+    private MovieList movieList;
+    private Movie whiplashMovie;
+    private Movie birdmanMovie;
+
+    @Before
+    public void setUp() throws Exception {
+        inputForSelectingMenu = "0\n";
+        byteArrayInputStream = new ByteArrayInputStream(inputForSelectingMenu.getBytes());
+        scanner = new Scanner(byteArrayInputStream);
+
+        outputStream = new ByteArrayOutputStream();
+        printer = new Printer(outputStream);
+
+        menuList = new MenuList();
+        menuList.add(new ListAllMovies());
+        menuList.add(new Quit());
+
+        bookList = mock(BookList.class);
+        movieList = new MovieList();
+        whiplashMovie = new Movie(1, "Whiplash", "Damien Chazelle", 2014, Rating.NINE);
+        birdmanMovie = new Movie(2, "BirdMan", "Alejandro González Iñárritu", 2014, Rating.TEN);
+        movieList = new MovieList();
+        movieList.add(whiplashMovie);
+        movieList.add(birdmanMovie);
+
+        library = new Library(bookList, movieList, printer);
+
+        bibliotecaApp = new BibliotecaApp(printer, scanner, menuList);
+    }
+
+    @Test
+    public void testSelectionOfListOfMovies() throws Exception, ItemNotFoundException, ItemIsNotAvailableForCheckOut, InputValidationException, ItemCanNotBeReturned {
+        inputForSelectingBook = "1\n";
+        byteArrayInputStream = new ByteArrayInputStream(inputForSelectingBook.getBytes());
+        scanner = new Scanner(byteArrayInputStream);
+        bibliotecaApp = new BibliotecaApp(printer, scanner, menuList);
+        bibliotecaApp.run(library);
+
+        String expectedOutPut = StringUtil.getOutputString(
+                "Welcome To Biblioteca",
+                "-----------------------------------------------------------------------------",
+                "1. List Movies",
+                "2. Quit",
+                "Select Option: ",
+                "-----------------------------------------------------------------------------",
+                "|1       |Whiplash                                                        |Damien Chazelle                 |2014|NINE",
+                "|2       |BirdMan                                                         |Alejandro González Iñárritu     |2014|TEN",
+                "",
+                "-----------------------------------------------------------------------------",
+                "1. List Movies",
+                "2. Quit",
+                "Select Option: "
+        );
+
+        assertThat(outputStream.toString(),is(expectedOutPut));
+    }
+}
