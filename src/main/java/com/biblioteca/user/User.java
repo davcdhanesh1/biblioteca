e15f6debf13d8c47637e06b34e5d9346698ca9bd
Dhanesh Arole <dhanesha@thoughtworks.com>
2015-03-03 12:59:27 +0530

    Add item API on user

--- src/main/java/com/biblioteca/user/User.java
+++ src/main/java/com/biblioteca/user/User.java
@@ -9,19 +9,27 @@ public class User {
     private final String name;
     private final String email;
     private final String phoneNumber;
-    private final ArrayList<Item> items;
+    private ArrayList<Item> borrowedItems;
 
-    public User(String libraryNumber, String name, String email, String phoneNumber, ArrayList<Item> items) {
+    public User(String libraryNumber, String name, String email, String phoneNumber) {
 
         this.libraryNumber = libraryNumber;
         this.name = name;
         this.email = email;
         this.phoneNumber = phoneNumber;
-        this.items = items;
+        this.borrowedItems = new ArrayList<Item>();
     }
 
     @Override
     public String toString() {
         return String.format("|%-8s|%-32s|%-32s|%-10s", libraryNumber, name, email, phoneNumber);
+    }
+
+    public void addItem(Item item) {
+        borrowedItems.add(item);
+    }
+
+    public ArrayList<Item> borrowedItems() {
+        return borrowedItems;
     }
 }
--- src/test/java/com/biblioteca/user/UserTest.java
+++ src/test/java/com/biblioteca/user/UserTest.java
@@ -1,19 +1,41 @@
 package com.biblioteca.user;
 
-import com.biblioteca.item.Item;
+import com.biblioteca.item.book.Book;
+import com.biblioteca.item.movie.Movie;
+import com.biblioteca.item.movie.Rating;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class UserTest {
+    private User user;
+    private Book book;
+    private Movie movie;
+
+    @Before
+    public void setUp() throws Exception {
+        user = new User("777-4445", "Dhanesh", "davcdhanesh1@gmail.com", "9096904102");
+        book = new Book(1, "A pedagogy of opression", "Paulo Freire", 1987);
+        movie = new Movie(1, "Whiplash", "Damien Chazelle", 2014, Rating.TEN);
+    }
 
     @Test
     public void shouldReturnTrueIfUserMatches() throws Exception {
-        User user = new User("777-4445", "Dhanesh", "davcdhanesh1@gmail.com", "9096904102", new ArrayList<Item>(10));
         String expectedUserString = "|777-4445|Dhanesh                         |davcdhanesh1@gmail.com          |9096904102";
         assertThat(user.toString(),is(expectedUserString));
     }
+
+    @Test
+    public void testAddItem() throws Exception {
+        user.addItem(book);
+        user.addItem(movie);
+
+        assertThat(user.borrowedItems(),hasItem(book));
+        assertThat(user.borrowedItems(),hasItem(movie));
+    }
+
+
 }
