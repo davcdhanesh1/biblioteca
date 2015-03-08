564dba86110a156143b1cc3c5ada6a9bf0cc590b
Dhanesh Arole <dhanesha@thoughtworks.com>
2015-03-09 00:02:16 +0530

    Creating predefined admin user

--- src/main/java/com/biblioteca/Main.java
+++ src/main/java/com/biblioteca/Main.java
@@ -35,7 +35,7 @@ public class Main {
         movieList.add(birdmanMovie);
 
         UserList userList = new UserList();
-        User dhanesh = User.customer("777-4445", "Dhanesh", "password", "davcdhanesh1@gmail.com", "9096904102");
+        User dhanesh = User.admin("777-4445", "Dhanesh", "password", "davcdhanesh1@gmail.com", "9096904102");
         User frank = User.customer("777-4446", "frank", "password", "frank.underwood@gmail.com", "9096904102");
         userList.add(dhanesh);
         userList.add(frank);
