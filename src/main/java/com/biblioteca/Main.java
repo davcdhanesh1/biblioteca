5af36e4d16c3f2b2b8f70df5be499ce8f7c3d6a5
Dhanesh Arole <dhanesha@thoughtworks.com>
2015-03-08 23:57:26 +0530

    Syncing up Main with other code

--- src/main/java/com/biblioteca/Main.java
+++ src/main/java/com/biblioteca/Main.java
@@ -22,15 +22,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws InvalidItemException, ItemIsNotAvailableForCheckOut, ItemCanNotBeReturned, InputValidationException, InvalidLibraryAndPasswordCombination {
-        MenuOptionList menuList = new MenuOptionList();
-        menuList.add(new ListAllBook());
-        menuList.add(new ListAllMovies());
-        menuList.add(new CheckOutBook());
-        menuList.add(new CheckOutMovie());
-        menuList.add(new ReturnBook());
-        menuList.add(new Login());
-        menuList.add(new Quit());
-
         BookList bookList = new BookList();
         Book harryPotterAndPhilosophersStoneBook = new Book(10, "Harry potters and philosophers Stone", "J K Rowling", 1987);
         Book harryPotterAndChambersOfSecretsBook = new Book(11, "Harry potters and chambers of secrets", "J K Rowling", 1987);
